c7e80880fc7b5f6c7ee3fefc99dfabdba5cddf73
James Vickery <dev@jmsv.me>
2022-08-23 01:54:56 +0100

    init sanity cms

---
+++ cms/schemas/schema.js
@@ -0,0 +1,9 @@
+import createSchema from "part:@sanity/base/schema-creator";
+import schemaTypes from "all:part:@sanity/base/schema-type";
+
+import teamMember from "./teamMember";
+
+export default createSchema({
+  name: "default",
+  types: schemaTypes.concat([teamMember]),
+});
---
+++ cms/schemas/teamMember.js
@@ -0,0 +1,12 @@
+export default {
+  name: "teamMember",
+  type: "document",
+  title: "Team Member",
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Name",
+    },
+  ],
+};
