1119344fb74c5dc523ba0ce54cd7918e1dc0c851
James Vickery <dev@jmsv.me>
2022-08-26 02:18:00 +0100

    cute footer

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -1,4 +1,7 @@
 import { FC } from "react";
+import Image from "next/image";
+
+import fusionHeart from "../../public/fusion-heart.png";
 
 import { Socials } from "../molecules/Socials";
 
@@ -6,7 +9,17 @@ export const Footer: FC = () => {
   return (
     <footer className="bg-slate-200 dark:bg-slate-800 text-slate-600 dark:text-slate-300 mt-auto">
       <div className="container mx-auto p-4 grid grid-cols-2 gap-4">
-        <div>© Fusion</div>
+        <div className="flex flex-row flex-wrap items-center gap-1">
+          Made with
+          <Image
+            alt="Fusion heart"
+            src={fusionHeart}
+            layout="fixed"
+            width={20}
+            height={20}
+          />
+          by Fusion in Birmingham
+        </div>
 
         <div className="flex flex-row items-center justify-end">
           <Socials />
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,9 +9,13 @@ const Home: NextPage = () => (
     <Hero />
 
     <div className="container mx-auto p-4">
-      <h1 className="text-5xl font-bold py-4">Fusion Meetup</h1>
+      <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold py-4">
+        Fusion Meetup
+      </h1>
 
-      <p className="pb-2">A quarterly tech meetup held in Birmingham city centre</p>
+      <p className="pb-2">
+        A quarterly tech meetup held in Birmingham city centre
+      </p>
 
       <div className="flex flex-row gap-4 flex-wrap pb-12 py-2">
         <Button>Blue button</Button>
