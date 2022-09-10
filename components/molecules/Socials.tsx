8c6a862685f870b64a20f429a8030b3b3ad09294
James Vickery <dev@jmsv.me>
2022-09-10 17:00:05 +0100

    footer socials update

--- components/molecules/Socials.tsx
+++ components/molecules/Socials.tsx
@@ -1,5 +1,6 @@
+import clsx from "clsx";
 import type { FC, ReactNode } from "react";
-import { FaFacebook, FaTwitter, FaInstagram, FaMeetup } from "react-icons/fa";
+import { FaFacebook, FaTwitter, FaMeetup, FaLinkedin } from "react-icons/fa";
 
 interface SocialLinkItem {
   href: string;
@@ -16,27 +17,35 @@ const socialLinks: SocialLinkItem[] = [
     icon: <FaTwitter />,
   },
   {
-    href: "https://www.facebook.com/fusionmeetup",
-    icon: <FaFacebook />,
+    href: "https://www.linkedin.com/company/technical-team-solutions/",
+    icon: <FaLinkedin />,
   },
   {
-    href: "https://www.instagram.com/fusionmeetup",
-    icon: <FaInstagram />,
+    href: "https://www.facebook.com/fusionmeetup",
+    icon: <FaFacebook />,
   },
 ];
 
 export const Socials: FC = () => {
   return (
-    <div className="flex flex-row gap-6 text-2xl">
+    <div className="flex flex-row text-2xl">
       {socialLinks.map(({ href, icon }) => (
         <a
           key={href}
           href={href}
           target="_blank"
           rel="noopener noreferrer"
-          className="transition-opacity opacity-75 hover:opacity-100"
+          className="group p-2 focus:outline-none"
         >
-          {icon}
+          <div
+            className={clsx(
+              "h-10 w-10 rounded-md flex items-center justify-center",
+              "transition-opacity group-hover:bg-slate-300 dark:group-hover:bg-slate-700",
+              "group-focus:outline outline-2 outline-slate-400 dark:outline-slate-500"
+            )}
+          >
+            {icon}
+          </div>
         </a>
       ))}
     </div>
--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -6,8 +6,8 @@ import fusionHeart from "../../public/fusion-heart.png";
 export const Footer: React.FC = () => {
   return (
     <footer className="z-2 bg-slate-200 dark:bg-slate-800 text-slate-600 dark:text-slate-300 mt-auto">
-      <div className="container mx-auto p-4 py-6 flex flex-row flex-wrap gap-x-4 gap-y-6">
-        <div className="flex flex-row flex-wrap items-center gap-1">
+      <div className="container mx-auto p-4 flex flex-row flex-wrap gap-x-4">
+        <div className="flex flex-row flex-wrap items-center gap-1 py-2">
           Made with
           <Image
             alt="Fusion heart"
