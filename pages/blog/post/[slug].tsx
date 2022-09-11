259c9c9f1556e5e4d7fbeb4a9e0ad849e911f4ef
James Vickery <dev@jmsv.me>
2022-09-11 01:04:56 +0100

    blog post author

--- pages/blog/post/[slug].tsx
+++ pages/blog/post/[slug].tsx
@@ -1,10 +1,12 @@
 import type { GetStaticProps, NextPage } from "next";
 
 import { BlogPost } from "../../../types/cms/Blog";
+import { getBlogPostBySlug, getBlogPostsSlugs } from "../../../lib/cms/queries";
 import { Layout } from "../../../components/organisms/Layout";
 import { Heading } from "../../../components/atoms/Heading";
 import { SanityContent } from "../../../components/atoms/SanityContent";
-import { getBlogPostBySlug, getBlogPostsSlugs } from "../../../lib/cms/queries";
+import { BlogAuthor } from "../../../components/blog/BlogAuthor";
+import dayjs from "dayjs";
 
 interface BlogPostPageProps {
   blogPost?: BlogPost;
@@ -16,8 +18,15 @@ const BlogPostPage: NextPage<BlogPostPageProps> = ({ blogPost }) => {
   return (
     <Layout title={`${blogPost.title} – Blog`}>
       <div className="max-w-[640px] xl:max-w-[800px] mx-auto p-4">
-        <div className="py-6">
+        <div className="py-6 flex flex-col gap-6">
           <Heading level={1}>{blogPost.title}</Heading>
+
+          <div className="p-4 pl-5 pr-6 rounded-md bg-white dark:bg-slate-800 shadow flex flex-row items-center justify-between gap-4 flex-wrap">
+            {blogPost.author ? (
+              <BlogAuthor teamMember={blogPost.author} enableTwitterLink />
+            ) : null}
+            Published on {dayjs(blogPost.publishedAt).format("Do MMMM, YYYY")}
+          </div>
         </div>
 
         <div className="pt-4 pb-16">
