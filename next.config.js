9dc86fad268630f737a8376e7320917ea9a2dc8d
James Vickery <dev@jmsv.me>
2022-09-11 16:42:02 +0100

    cms redirect

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
   images: {
     domains: ["cdn.sanity.io"],
   },
+  redirects: async () => [
+    {
+      source: "/cms",
+      destination: "https://fusion-meetup-web.sanity.studio/",
+      permanent: true,
+    },
+  ],
 };
 
 module.exports = nextConfig;
