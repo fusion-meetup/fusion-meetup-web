ba219438297569038d9fc5aa535fe2250d4cdc4d
James Vickery <dev@jmsv.me>
2022-09-13 23:51:02 +0100

    about schema fields

--- sanity-studio/schemas/about.js
+++ sanity-studio/schemas/about.js
@@ -2,17 +2,26 @@ export default {
   name: "about",
   title: "About Fusion",
   type: "document",
-  // TODO: Use fields from AboutFusion content object
   fields: [
     {
       name: "title",
-      title: "Site Title",
+      title: "About Fusion Title",
       type: "string",
     },
     {
       name: "description",
       title: "Site Description",
-      type: "text",
+      type: "content",
+    },
+    {
+      name: "quote",
+      title: "Quote",
+      type: "content",
+    },
+    {
+      name: "buttonText",
+      title: "Button Text",
+      type: "string",
     },
   ],
 };
