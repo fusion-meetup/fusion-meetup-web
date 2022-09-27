5df2ee3726728cde0c61c3271ddac89629cb41a3
James Vickery <dev@jmsv.me>
2022-09-27 01:07:03 +0100

    eventbrite widget design and fix loading on nav

--- components/atoms/Eventbrite.tsx
+++ components/atoms/Eventbrite.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Script from "next/script";
 
 interface EventbriteProps {
@@ -5,25 +6,45 @@ interface EventbriteProps {
 }
 
 const Eventbrite: React.FC<EventbriteProps> = ({ eventbriteLink }) => {
-  if (!eventbriteLink) return null;
-  const eventbriteId = new URL(String(eventbriteLink)).pathname.replace(
-    /[^0-9]+/gi,
-    ""
-  );
+  const [ebScriptIsLoaded, setEbScriptIsLoaded] = useState(false);
+
+  const eventbriteId = eventbriteLink
+    ? new URL(String(eventbriteLink)).pathname.replace(/[^\d]+/gi, "")
+    : null;
+
   const iframeContainerId = `eventbrite-widget-container-${eventbriteId}`;
+
+  useEffect(() => {
+    // Need to set and check window.ebScriptIsLoaded because Script onLoad only
+    // fires once - without this, the Eventbrite widget won't load in when
+    // navigating (client-side navigation) away and back to the page
+    const scriptsReady = ebScriptIsLoaded || window.ebScriptIsLoaded;
+    if (!scriptsReady || !eventbriteId) return;
+
+    window.EBWidgets?.createWidget({
+      widgetType: "checkout",
+      eventId: eventbriteId,
+      iframeContainerId,
+    });
+  }, [ebScriptIsLoaded, eventbriteId, iframeContainerId]);
+
+  if (!eventbriteLink) return null;
+
   return (
     <>
-      <div id={iframeContainerId}></div>
+      <div className="rounded-xl bg-white dark:bg-slate-600 bg-opacity-50 dark:bg-opacity-50">
+        <div className="text-xl md:text-2xl text-center p-2 font-bold">
+          Get tickets
+        </div>
+
+        <div id={iframeContainerId} className="eventbrite-container" />
+      </div>
+
       <Script
         id="eventbrite-js"
         src="https://www.eventbrite.co.uk/static/widgets/eb_widgets.js"
-        onLoad={() => {
-          window.EBWidgets.createWidget({
-            widgetType: "checkout",
-            eventId: eventbriteId,
-            iframeContainerId,
-          });
-        }}
+        onLoad={() => setEbScriptIsLoaded((window.ebScriptIsLoaded = true))}
+        strategy="lazyOnload"
       />
     </>
   );
--- types/index.d.ts
+++ types/index.d.ts
@@ -2,6 +2,7 @@ export {};
 
 declare global {
   interface Window {
-    EBWidgets: any;
+    EBWidgets?: any;
+    ebScriptIsLoaded?: boolean;
   }
 }
