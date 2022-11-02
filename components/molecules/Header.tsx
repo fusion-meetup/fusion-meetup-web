3aba1c8ded906665380bcfe6e8f8d0f92a17df4d
James Vickery <dev@jmsv.me>
2022-11-02 21:23:04 +0000

    tickets coming soon disabled button

--- components/molecules/Header.tsx
+++ components/molecules/Header.tsx
@@ -5,10 +5,9 @@ import { SiteMenu } from "./SiteMenu";
 
 export const Header = () => {
   return (
-    // TODO: Transparent at top of page?
     <header
       className={clsx(
-        "fixed z-10 w-screen backdrop-blur z-10 shadow-sm dark:shadow-md",
+        "fixed z-10 w-screen backdrop-blur shadow-sm dark:shadow-md",
         "bg-slate-100 dark:bg-slate-800 bg-opacity-80 dark:bg-opacity-40",
         "flex flex-row justify-between"
       )}
